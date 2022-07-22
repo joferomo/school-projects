e5e8514ac9413100fb69574b6e488048a8342a93
joferomo <88719142+joferomo@users.noreply.github.com>
2022-07-22 11:29:46 +0000

    Update hex-to-binary.c
    Made it so you can change how many hexadecimal digits can be read (just change TAM_HEX to anything >= 3). Also fixed an issue where if you typed too many characters the remaining characters in the buffer would be read before you could type anything else.

--- hex-to-binary.c
+++ hex-to-binary.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#define TAM_HEX 7
-#define TAM_BIN 24
+#define TAM_HEX 8
+#define TAM_BIN (TAM_HEX-2)*4
 
 int main(void){
 	int getDecimal(char numHex);
@@ -13,19 +13,24 @@ int main(void){
 
 	do{
 		ok = 1;
-		printf("Entre com um valor hexadecimal de até 6 digitos:\n");
-		fgets(numHex, 7, stdin);
+		printf("Entre com um valor hexadecimal de até %d digitos:\n", TAM_HEX-2);
+		fgets(numHex, TAM_HEX, stdin);
 		hexLength = strlen(numHex);
 		if(numHex[hexLength-1] == '\n')
 		{
 			numHex[--hexLength] = '\0';
-		}
-		for(i = 0; i < hexLength; i++){
-			if((numHex[i] < 48 || numHex[i] > 57) && (numHex[i] < 65 || numHex[i] > 70) && (numHex[i] < 97 || numHex[i] > 102)){
-				ok = 0;
-				printf("\n");
-				break;
+			for(i = 0; i < hexLength; i++){
+				if((numHex[i] < 48 || numHex[i] > 57) && (numHex[i] < 65 || numHex[i] > 70) && (numHex[i] < 97 || numHex[i] > 102)){
+					ok = 0;
+					printf("\n");
+					break;
+				}
 			}
+		}
+		else{
+			ok = 0;
+			printf("\n");
+			while(getchar() != '\n');
 		}
 	} while(ok == 0);
 
